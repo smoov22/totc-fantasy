dcd58241f24ec1f4490681e4f991572cd1cb624f
Kyle <kam3634@rit.edu>
2022-03-06 07:00:25 -0500

    artist name

--- Updater.java
+++ Updater.java
@@ -11,11 +11,11 @@ public class Updater {
         for (Artists artists: Artists.values()) {
             Set<Integer> points = new HashSet<>();
             int total = 0;
-            System.out.println("Enter a placement number on the Hot 100 or 0 when done.");
+            System.out.println("Enter a placement number for " + artists.toString() + " on the Hot 100 or 0 when done.");
             int placement = scanner.nextInt();
             while (placement != 0) {
                 points.add(101-placement);
-                System.out.println("Enter a placement number on the Hot 100 or 0 when done.");
+                System.out.println("Enter a placement number for " + artists.toString() + " on the Hot 100 or 0 when done.");
                 placement = scanner.nextInt();
             }
             for (int point: points) {
