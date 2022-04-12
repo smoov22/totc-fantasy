50e31fb85ec12ea4eb19166f264f256bfd39a99b
Kyle <kam3634@rit.edu>
2022-04-12 07:54:30 -0400

    4-16

--- year_end.py
+++ year_end.py
@@ -26,7 +26,7 @@ def remove_first_three_chars (filename):
     return string
 
 def main():
-    output=format("hot100/4-9.txt")
+    output=format("hot100/4-16.txt")
     file = open("temp.txt", "w")
     file.write(output)
 
