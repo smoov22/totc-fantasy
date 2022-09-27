d927506843b2c86d1e970024563db1764bdee33b
Kyle <kam3634@rit.edu>
2022-09-27 08:01:34 -0400

    10-1

--- alt-formatter.py
+++ alt-formatter.py
@@ -29,7 +29,7 @@ def remove_first_three_chars (filename): # removes first three characters from a
     return string
 
 def main(): 
-    output=format("hot100/9-24.txt") # changed week by week to represent the incoming file
+    output=format("hot100/10-1.txt") # changed week by week to represent the incoming file
     file = open("temp.txt", "w")
     file.write(output)
 
