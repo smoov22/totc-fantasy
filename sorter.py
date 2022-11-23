8f8b50a8e8314d5b60e9e7e4cac148865045fe7a
Kyle <kam3634@rit.edu>
2022-11-22 19:56:33 -0500

    11-26 and starting to try to make a cli preference revealer

---
+++ sorter.py
@@ -0,0 +1,34 @@
+import random
+def sorter (filename):
+    with open(filename) as result:
+        options = list()
+        for row in result:
+            options.append(row)
+        random.shuffle(options)
+        sorted = dict()
+        i = 0
+        while i < len(options)-1:
+            option1 = options[i]
+            option2 = options[i + 1]
+            answer = input("Which do you prefer? 1 for " + option1 + " or 2 for " + option2)
+            if (answer == "1"):
+                sorted.update({option1:1})
+                sorted.update({option2:-1})
+            if (answer == "2"):
+                sorted.update({option1:-1})
+                sorted.update({option2:1})
+            i = i + 2
+        if len(options) % 2 != 0:
+            sorted.update ({options[len(options) - 1]:0})
+        for item in sorted:
+            print(item + " : " + str(sorted.get(item)))
+        # while there are any keys in the dictionary with the same value, find the highest value that at least two 
+         
+        
+        
+
+def main():
+    text = input('Enter filename: ')
+    sorter(text)
+
+main()
